9f50ef28f8f26d6f3a3882a67864872907e45177
Ozgun Atacan <ozgun.atacan@gmail.com>
2020-01-12 15:16:24 +0300

    fixing the build for code coverage

--- mix.exs
+++ mix.exs
@@ -14,7 +14,8 @@ defmodule Exbrainz.MixProject do
         coveralls: :test,
         "coveralls.detail": :test,
         "coveralls.post": :test,
-        "coveralls.html": :test
+        "coveralls.html": :test,
+        "coveralls.github": :test
       ]
     ]
   end
